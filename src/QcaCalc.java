8878e8986ed6cf73051daaa9ac2907b2009e4e11
jasoncush1076 <150550251+jasoncush1076@users.noreply.github.com>
2023-12-01 14:15:57 +0000

    Javadoc for QcaCalc class

--- src/QcaCalc.java
+++ src/QcaCalc.java
@@ -1,12 +1,20 @@
 import java.util.Map;
 
+/**A class which provides methods for claculating QCA based on grades. It maps grades to the corresponding QCA
+    * Class gets information from CSV file */
 public class QcaCalc {
 
+//creates object of the CsvReader class
     static CsvReader reader = new CsvReader();
+    
+//maps grades to corresponding QCA; reads grades from CSV file
     static Map<String, Double> gradeQcaMap = reader.readGrade("./src/data/grades.csv");
 
+/**Calculates QCA for a given set of grades
+    *@param grades -the grades to be calculated
+    *@return the QCA */
     public static double getCummulativeQca ( String ... grades) {
-        //I used to Varargs here instead of method overloading for more clean and readable code
+        //We used to Varargs here instead of method overloading for more clean and readable code
         int numModules = grades.length;
         double cummulativeQca = 0.00;
 
